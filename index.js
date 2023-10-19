4bbac45989ccb4124a7ba512f14a76eaa4427805
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-10-19 06:09:26 +0300

    Practise Makes Perfect

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+function createEmployeeRecord(array){
+    const empObj = {
+        firstName:array[0],
+        familyName:array[1],
+        title:array[2],
+        payPerHour:array[3],
+        timeInEvents:[],
+        timeOutEvents:[]
+    }
+
+    return empObj;
+}
+
+function createEmployeeRecords(arrayOfArrays){
+    const employeeRecords = arrayOfArrays.map(employeData => createEmployeeRecord(employeData))
+    return employeeRecords
+}
+
+  
