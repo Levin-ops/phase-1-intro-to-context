ad43ae4d8a493560911c09d490374e6443b2dbba
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-10-19 06:34:02 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -17,4 +17,46 @@ function createEmployeeRecords(arrayOfArrays){
     return employeeRecords
 }
 
-  
+function createTimeInEvent(employee, dateStamp){
+    const [date, time] = dateStamp.split(' ');
+    employee.timeInEvents.push({
+        type: `TimeIn`,
+        hour: parseInt(time),
+        date: date,
+    })
+
+    return employee;
+}
+
+function createTimeOutEvent(employee, dateStamp){
+    const [date, time] = dateStamp.split(' ');
+    employee.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(time),
+        date: date,
+    })
+    return employee;
+}
+
+function hoursWorkedOnDate(employee, date){
+    const timeInEvent = employee.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = employee.timeOutEvents.find(event => event.date === date);
+    const hoursWorking = (timeOutEvent.hour - timeInEvent.hour)/100;
+
+    return hoursWorking
+}
+
+function wagesEarnedOnDate(employee, date){
+    const workingHours = hoursWorkedOnDate(employee, date);
+    return workingHours * employee.payPerHour;
+}
+
+function allWagesFor(employee){
+    const workingDates = employee.timeInEvents.map(event => event.date);
+    const totalPay = workingDates.reduce((total, date) => total + wagesEarnedOnDate(employee, date), 0)
+    return totalPay;
+}
+
+function calculatePayroll(employees){
+    return employees.reduce((totalWages,employee) => totalWages + allWagesFor(employee),0)
+}
